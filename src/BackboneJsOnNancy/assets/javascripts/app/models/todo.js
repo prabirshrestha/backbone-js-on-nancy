5b46d88d748acd37c6f9607cd59a44e1b240bdd7
prabirshrestha <mail@prabir.me>
2012-03-17 13:33:08 -0400

    models/todo.js

---
+++ src/BackboneJsOnNancy/assets/javascripts/app/models/todo.js
@@ -0,0 +1,11 @@
+﻿App.Models.Todo = Backbone.Model.extend({
+    defaults: function () {
+        return {
+            done: false
+        };
+    },
+
+    toggle: function () {
+        this.save({ done: !this.get('done') });
+    }
+});
