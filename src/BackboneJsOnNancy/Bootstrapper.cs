1ffeffaad191d89c26ef2af306269572515b403d
prabirshrestha <mail@prabir.me>
2012-03-17 20:40:12 -0400

    added configuration for CassetteStartup.ShouldOptimizeOutput

--- src/BackboneJsOnNancy/Bootstrapper.cs
+++ src/BackboneJsOnNancy/Bootstrapper.cs
@@ -1,9 +1,16 @@
 ﻿
 namespace BackboneJsOnNancy
 {
+    using Cassette.Nancy;
     using Nancy;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        protected override void ApplicationStartup(TinyIoC.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            CassetteStartup.ShouldOptimizeOutput = false;
+        }
     }
 }
