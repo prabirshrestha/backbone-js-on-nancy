3cd2451f5ab49130603ddd8c7f2ce5d6bd75fc04
prabirshrestha <mail@prabir.me>
2012-04-14 12:53:18 -0400

    AuthenticationModule /login /logout /register

---
+++ src/BackboneJsOnNancy/Modules/AuthenticationModule.cs
@@ -0,0 +1,15 @@
+﻿namespace BackboneJsOnNancy.Modules
+{
+    using Nancy;
+
+    public class AuthenticationModule : NancyModule
+    {
+        public AuthenticationModule()
+        {
+            Get["/login"] = x => "login";
+            Get["/logout"] = x => "logout";
+
+            Get["/register"] = x => "register";
+        }
+    }
+}
