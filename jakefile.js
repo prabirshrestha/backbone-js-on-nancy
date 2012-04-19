cf4f583949c359a142cf6b4f8df767c1901c7fd0
prabirshrestha <mail@prabir.me>
2012-04-18 22:56:53 -0400

    jake build clean

--- jakefile.js
+++ jakefile.js
@@ -1,3 +1,26 @@
 var njake = require('./src/njake'),
 	msbuild = njake.msbuild,
 	nuget = njake.nuget;
+
+msbuild.setDefaults({
+	version: 'net4.0',
+	processor: 'x86',
+	Configuration: ['Release']
+})
+
+
+task('default', ['clean', 'build'])
+
+task('build', function () {
+	msbuild({
+		file: 'src/BackboneJsOnNancy.sln',
+		targets: ['Build']
+	});
+});
+
+task('clean', function () {
+	msbuild({
+		file: 'src/BackboneJsOnNancy.sln',
+		targets: ['Clean']
+	})
+})
