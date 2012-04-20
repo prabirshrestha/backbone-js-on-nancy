af0e3a343259111486b73565c6cc60eed8dc126b
prabirshrestha <mail@prabir.me>
2012-04-19 22:55:57 -0400

    aded UserMapper and updated nancfx/nancy-prabir

--- src/BackboneJsOnNancy.Web/Bootstrapper.cs
+++ src/BackboneJsOnNancy.Web/Bootstrapper.cs
@@ -4,17 +4,40 @@ namespace BackboneJsOnNancy.Web
     using System;
     using System.Collections.Generic;
     using Nancy;
+    using Nancy.Authentication.Forms;
     using Nancy.Bootstrapper;
+    using TinyIoC;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
-        protected override void ApplicationStartup(TinyIoC.TinyIoCContainer container, IPipelines pipelines)
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
         {
             base.ApplicationStartup(container, pipelines);
 
 #if !DEBUG
             Cassette.Nancy.CassetteStartup.ShouldOptimizeOutput = true;
 #endif
+            container.Register<IUserMapper, UserMapper>();
+
+        }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            base.RequestStartup(container, pipelines, context);
+
+            FormsAuthenticationRequestStartup(container, pipelines, context);
+        }
+
+        private void FormsAuthenticationRequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+
+            var config = new FormsAuthenticationConfiguration
+                             {
+                                 RedirectUrl = "~/login",
+                                 UserMapper = container.Resolve<IUserMapper>()
+                             };
+
+            FormsAuthentication.Enable(pipelines, config);
         }
 
         protected override NancyInternalConfiguration InternalConfiguration
---
+++ src/BackboneJsOnNancy/UserMapper.cs
@@ -0,0 +1,19 @@
+﻿namespace BackboneJsOnNancy
+{
+    using System;
+    using Nancy.Authentication.Forms;
+    using Nancy.Security;
+
+    public class UserMapper : IUserMapper
+    {
+        public UserMapper()
+        {
+            
+        }
+
+        public IUserIdentity GetUserFromIdentifier(Guid identifier)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
