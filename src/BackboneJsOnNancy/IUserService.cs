2a4903f0eba3a424cb635b6c3b58e0c748cb8f80
prabirshrestha <mail@prabir.me>
2012-04-20 21:18:20 -0400

    IUserService

---
+++ src/BackboneJsOnNancy/IUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace BackboneJsOnNancy
+{
+    using System;
+    using Nancy.Authentication.Forms;
+
+    public interface IUserService : IUserMapper
+    {
+        Guid? Authenticate(string username, string password);
+    }
+}
