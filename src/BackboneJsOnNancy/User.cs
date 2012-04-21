205933ef1fc251f4ec15433422c62b02dd98f92a
prabirshrestha <mail@prabir.me>
2012-04-20 20:22:03 -0400

    User.cs

---
+++ src/BackboneJsOnNancy/User.cs
@@ -0,0 +1,16 @@
+﻿namespace BackboneJsOnNancy
+{
+    using System.Collections.Generic;
+    using Nancy.Security;
+
+    public class User : IUserIdentity
+    {
+        public User()
+        {
+            Claims = new List<string>();
+        }
+
+        public string UserName { get; set; }
+        public IEnumerable<string> Claims { get; set; }
+    }
+}
