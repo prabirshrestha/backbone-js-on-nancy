6d17b4d3f0661340b1ce9a1b87a9b238af620d14
prabirshrestha <mail@prabir.me>
2012-05-05 12:06:00 -0400

    dummy tests

---
+++ src/BackboneJsOnNancy.Web.Tests/TestBootstrapper.cs
@@ -0,0 +1,32 @@
+﻿namespace BackboneJsOnNancy.Web.Tests
+{
+    using System;
+    using System.IO;
+    using Nancy.Testing;
+    using Nancy.Testing.Fakes;
+
+    public class TestBootstrapper : Bootstrapper
+    {
+        protected override Type RootPathProvider
+        {
+            get
+            {
+                var assemblyFilePath =
+                    new Uri(typeof(Bootstrapper).Assembly.CodeBase).LocalPath;
+
+                var assemblyPath =
+                    Path.GetDirectoryName(assemblyFilePath);
+
+                var rootPath =
+                    PathHelper.GetParent(assemblyPath, 3);
+
+                rootPath =
+                    Path.Combine(rootPath, @"BackboneJsOnNancy.Web");
+
+                FakeRootPathProvider.RootPath = rootPath;
+
+                return typeof(FakeRootPathProvider);
+            }
+        } 
+    }
+}
---
+++ src/BackboneJsOnNancy.Web.Tests/Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace BackboneJsOnNancy.Web.Tests
+{
+    using Nancy.Testing;
+    using Xunit;
+
+    public class Tests
+    {
+        [Fact]
+        public void Test()
+        {
+            var bootstrapper = new TestBootstrapper();
+            var browser = new Browser(bootstrapper);
+
+            browser.Get("/login", with => with.HttpRequest());
+        }
+    }
+}
