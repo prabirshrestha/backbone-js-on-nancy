4b45cc7bb36f09501932ff3cde0561e7d2854680
prabirshrestha <mail@prabir.me>
2012-05-10 18:50:18 -0400

    register.js view onFormSumbit disable buttons and clear password value

--- src/BackboneJsOnNancy.Web/assets/javascripts/public_app/views/authentication/register.js
+++ src/BackboneJsOnNancy.Web/assets/javascripts/public_app/views/authentication/register.js
@@ -3,17 +3,28 @@
     el: $('div[role="main"] form[role=register]'),
 
     events: {
+        'submit': 'onFormSubmit',        
         'click input[role=cancel]': 'cancel'
     },
 
     initialize: function () {
-        _.bindAll(this, 'cancel');
+        _.bindAll(this, 'onFormSubmit', 'cancel');
         this.inputs = this.$('input[type=email], input[type=password]');
     },
 
     cancel: function (e) {
         e.preventDefault();
         this.inputs.val('');
+    },
+
+    onFormSubmit: function (e) {
+        var self = this;
+        setTimeout(function () {
+            self.$('input[type=submit], input[type=button]')
+                .attr('disabled', 'disabled')
+                .addClass('disabled');
+            self.$('input[type=password]').val('');
+        }, 200);
     }
 
 });
