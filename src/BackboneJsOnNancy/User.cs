bcf746f33f3af01ab5b5783b43fb5af72f424c94
prabirshrestha <mail@prabir.me>
2012-09-21 18:58:56 -0400

    claims is Enumerable.Empty<string>

--- src/BackboneJsOnNancy/User.cs
+++ src/BackboneJsOnNancy/User.cs
@@ -1,10 +1,16 @@
 ﻿namespace BackboneJsOnNancy
 {
     using System.Collections.Generic;
+    using System.Linq;
     using Nancy.Security;
 
     public class User : IUserIdentity
     {
+        public User()
+        {
+            Claims = Enumerable.Empty<string>();
+        }
+
         public long Id { get; set; }
 
         public string UserName { get; set; }
